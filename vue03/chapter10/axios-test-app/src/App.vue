0153d1afaf2bd25dce8f35bd3f1fcb1cf1b52417
KIMKANGMIN <rkdals159@naver.com>
2024-06-03 12:38:13 +0900

    am0603

---
+++ vue03/chapter10/axios-test-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>console check</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const requestAPI = () => {
+  const url = 'https://localhost:3000/todos/1';
+  axios.get(url).then((response)=> {
+    console.log('# response : ', response);
+  });
+}
+
+requestAPI();
+
