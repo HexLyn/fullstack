6d9fcd3dc4319d2628d74bc675baed9d08048abf
KIMKANGMIN <rkdals159@naver.com>
2024-07-31 17:48:27 +0900

    88

--- JSPServlet/ex0307/src/main/java/org/scoula/ex03/BoardServlet.java
+++ JSPServlet/ex0307/src/main/java/org/scoula/ex03/BoardServlet.java
@@ -16,10 +16,14 @@ public class BoardServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.print("<html><body>");
 
+        //받아온 파라미터들의 이름만을 열거자로 만든다.
         Enumeration<String> enu = request.getParameterNames();
 
+        // hasMoreElements : 요소가 더 있는지 체크해서 없을때까지 돈다.
         while(enu.hasMoreElements()) {
+            //nextElement: 열거자를 돌면서 다음 요소를 가져옴
             String name= enu.nextElement();
+            // 열거자에 저장된 이름으로 해당 값들을 가져옴
             String value = request.getParameter(name);
             out.print(name + " : " + value + "<br>");
         }
